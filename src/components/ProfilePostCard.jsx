c162ade8bbb75eda9cae4b7c0b6f2717c0a853ec
Dinesh <dineshkerishna@gmail.com>
2025-07-01 11:56:13 +0800

    Firebase storage

--- src/components/ProfilePostCard.jsx
+++ src/components/ProfilePostCard.jsx
@@ -1,11 +1,12 @@
 import { useContext, useState } from "react";
 import { Button, Col, Image, Row } from "react-bootstrap";
 import { useDispatch } from "react-redux";
-import { likePost, removeLikeFromPost } from "../features/posts/postsSlice";
+import { deletePost, likePost, removeLikeFromPost } from "../features/posts/postsSlice";
 import { AuthContext } from "./AuthProvider";
+import UpdatePostModal from "./UpdatePostModal";
 
 export default function ProfilePostCard({ post }) {
-    const { content, id: postId } = post;
+    const { content, id: postId, imageUrl } = post;
     const [likes, setLikes] = useState(post.likes || []);
     const dispatch = useDispatch();
     const { currentUser } = useContext(AuthContext);
@@ -15,6 +16,11 @@ export default function ProfilePostCard({ post }) {
     const isLiked = likes.includes(userId);
 
     const pic = "https://pbs.twimg.com/profile_images/1587405892437221376/h167Jlb2_400x400.jpg";
+
+    const [showUpdateModal, setShowUpdateModal] = useState(false);
+
+    const handleShowUpdateModal = () => setShowUpdateModal(true);
+    const handleCloseUpdateModal = () => setShowUpdateModal(false);
 
     const handleLike = () => (isLiked ? removeFromLikes() : addToLikes());
 
@@ -30,6 +36,10 @@ export default function ProfilePostCard({ post }) {
         dispatch(removeLikeFromPost({ userId, postId }));
     };
 
+    const handleDelete = () => {
+        dispatch(deletePost({ userId, postId }));
+    };
+
     return (
         <Row className="p-3" style={{ borderTop: "1px solid #D3D3D3", borderBottom: "1px solid #D3D3D3 " }}>
             <Col sm={1}>
@@ -40,6 +50,7 @@ export default function ProfilePostCard({ post }) {
                 <strong>Dinesh</strong>
                 <span> @dinesh.kerishna : Apr 16</span>
                 <p>{content}</p>
+                <Image src={imageUrl} style={{ width: 150 }} />
                 <div className="d-flex justify-content-between">
                     <Button variant="light">
                         <i className="bi bi-chat"></i>
@@ -61,6 +72,21 @@ export default function ProfilePostCard({ post }) {
                     <Button variant="light">
                         <i className="bi bi-upload"></i>
                     </Button>
+                    <Button variant="light">
+                        <i
+                            className="bi bi-pencil-square"
+                            onClick={handleShowUpdateModal}
+                        ></i>
+                    </Button>
+                    <Button variant="light" onClick={handleDelete}>
+                        <i className="bi bi-trash"></i>
+                    </Button>
+                    <UpdatePostModal
+                        show={showUpdateModal}
+                        handleClose={handleCloseUpdateModal}
+                        postId={postId}
+                        originalPostContent={content}
+                    />
                 </div>
             </Col>
         </Row>
